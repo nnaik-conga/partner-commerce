53578a12b12f52079f43f9f102425931d716394e
APTTUS\nnaik <nnaik@conga.com>
2023-05-15 17:18:31 +0530

    Upgrade Activity for Sep22-June23

--- src/app/modules/quotes/request-quote-form/request-quote-form.component.ts
+++ src/app/modules/quotes/request-quote-form/request-quote-form.component.ts
@@ -3,7 +3,7 @@ import { BsDatepickerConfig } from 'ngx-bootstrap/datepicker';
 import { Observable, zip, of, combineLatest } from 'rxjs';
 import { take } from 'rxjs/operators';
 import { get } from 'lodash';
-import { AccountService, ContactService, UserService, Quote, QuoteService, PriceListService , Cart, Note, Account, Contact, PriceList } from '@congacommerce/ecommerce';
+import { AccountService, ContactService, UserService, Quote, QuoteService, PriceListService, Cart, Note, Account, Contact, PriceList } from '@congacommerce/ecommerce';
 import { LookupOptions } from '@congacommerce/elements';
 
 import * as moment from 'moment';
@@ -26,7 +26,7 @@ export class RequestQuoteFormComponent implements OnInit {
 
   shipToAccount$: Observable<Account>;
   billToAccount$: Observable<Account>;
-  priceList$:Observable<PriceList>;
+  priceList$: Observable<PriceList>;
   lookupOptions: LookupOptions = {
     primaryTextField: 'Name',
     secondaryTextField: 'Email',
@@ -37,21 +37,21 @@ export class RequestQuoteFormComponent implements OnInit {
   constructor(public quoteService: QuoteService,
     private accountService: AccountService,
     private userService: UserService,
-    private plservice:PriceListService,
+    private plservice: PriceListService,
     private contactService: ContactService) { }
 
   ngOnInit() {
     this.quote.Name = 'Test';
-    combineLatest(this.accountService.getCurrentAccount(), this.userService.me(),(this.cart.Proposald? this.quoteService.get([get(this.cart, 'Proposald.Id')]) : of(null)))
-    .pipe(take(1)).subscribe(([account, user, quote]) => {
+    combineLatest(this.accountService.getCurrentAccount(), this.userService.me(), (this.cart.Proposald ? this.quoteService.get([get(this.cart, 'Proposald.Id')]) : of(null)))
+      .pipe(take(1)).subscribe(([account, user, quote]) => {
         this.quote.ShipToAccount = account;
         this.quote.ShipToAccountId = account.Id;
         this.quote.BillToAccount = account;
-        this.quote.BillToAccountId =  account.Id;
-        this.quote.Account= get(this.cart,'Account');
+        this.quote.BillToAccountId = account.Id;
+        this.quote.Account = get(this.cart, 'Account');
         this.quote.Primary_Contact = get(user, 'Contact');
-        this.contactId = this.cart.Proposald?  get(quote[0],'Primary_ContactId') : get(user, 'ContactId');
-        if(get(this.cart, 'Proposald.Id')) {
+        this.contactId = this.cart.Proposald ? get(quote[0], 'Primary_ContactId') : get(user, 'ContactId');
+        if (get(this.cart, 'Proposald.Id')) {
           this.quote = get(this.cart, 'Proposald');
           this.comments = get(quote, '[0].Notes', []);
         }
@@ -60,9 +60,6 @@ export class RequestQuoteFormComponent implements OnInit {
       });
   }
 
-  /**
-   * This method adds comments to requesting quote.
-   */
   addComment() {
     if (this.quote) {
       this.quote.Description = this.note.Body;
@@ -70,29 +67,38 @@ export class RequestQuoteFormComponent implements OnInit {
     }
   }
 
-  /**
-   * @ignore
-   */
   quoteChange() {
     this.onQuoteUpdate.emit(this.quote);
   }
 
   shipToChange() {
-    this.shipToAccount$ = this.accountService.getAccount(this.quote.ShipToAccountId);
-    this.shipToAccount$.pipe(take(1)).subscribe((newShippingAccount) => {
-      this.quote.ShipToAccount = newShippingAccount;
+    if (this.quote.ShipToAccountId) {
+      this.shipToAccount$ = this.accountService.getAccount(this.quote.ShipToAccountId);
+      this.shipToAccount$.pipe(take(1)).subscribe((newShippingAccount) => {
+        this.quote.ShipToAccount = newShippingAccount;
+        this.onQuoteUpdate.emit(this.quote);
+      });
+    } else {
+      this.quote.ShipToAccount = null;
+      this.shipToAccount$= null;
       this.onQuoteUpdate.emit(this.quote);
-    });
+    }
   }
 
   billToChange() {
-    this.billToAccount$ = this.accountService.getAccount(this.quote.BillToAccountId);
-    this.billToAccount$.pipe(take(1)).subscribe((newBillingAccount) => {
-      this.quote.BillToAccount = newBillingAccount;
+    if (this.quote.BillToAccountId) {
+      this.billToAccount$ = this.accountService.getAccount(this.quote.BillToAccountId);
+      this.billToAccount$.pipe(take(1)).subscribe((newBillingAccount) => {
+        this.quote.BillToAccount = newBillingAccount;
+        this.onQuoteUpdate.emit(this.quote);
+      });
+    } else {
+      this.quote.BillToAccount = null;
+      this.billToAccount$= null;
       this.onQuoteUpdate.emit(this.quote);
-    });
-
+    }
   }
+
   getpriceList(){
     this.priceList$=this.plservice.getPriceList();
     this.priceList$.pipe(take(1)).subscribe((newPricelList) => {
@@ -100,11 +106,9 @@ export class RequestQuoteFormComponent implements OnInit {
       this.onQuoteUpdate.emit(this.quote);
     });
   }
- /**
-   * Event handler for when the primary contact input changes.
-   * @param event The event that was fired.
-   */
-  primaryContactChange() {
+
+primaryContactChange() {
+  if (this.contactId) {
     this.contactService.fetch(this.contactId)
       .pipe(take(1))
       .subscribe((newPrimaryContact: Contact) => {
@@ -112,6 +116,9 @@ export class RequestQuoteFormComponent implements OnInit {
         this.quote.Primary_ContactId = newPrimaryContact.Id;
         this.onQuoteUpdate.emit(this.quote);
       });
+  } else {
+    this.quote.Primary_Contact = null;
+    this.onQuoteUpdate.emit(this.quote);
   }
-
+}
 }
